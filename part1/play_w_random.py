3b5b3bbf970917d3f1ffa1eea1d586ce4fc7f5fd
Pankhuri Tomar <pankhuritomar711@gmail.com>
2026-03-24 11:42:57 +0530

    Create play_w_random.py

---
+++ part1/play_w_random.py
@@ -0,0 +1,7 @@
+# Importing the random library to generate random numbers
+import random
+
+# Using random.sample() to generate 10 unique random numbers
+numbers = random.sample(range(1, 11), 10)
+
+print("Random numbers:", numbers)
