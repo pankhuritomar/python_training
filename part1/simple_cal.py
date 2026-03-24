a88374c562bba3184daa44906e728ae7cd82c8f5
Pankhuri Tomar <pankhuritomar711@gmail.com>
2026-03-24 11:37:50 +0530

    Create simple_cal.py

---
+++ part1/simple_cal.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed")
+    else:
+        print("Result:", num1 / num2)
+else:
+    print("Invalid operator")
