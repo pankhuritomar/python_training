d640af54c3709ea73419cd9254a4cd91081eb58f
Pankhuri Tomar <pankhuritomar711@gmail.com>
2026-03-24 11:38:33 +0530

    Create string_manip.py

---
+++ part1/string_manip.py
@@ -0,0 +1,6 @@
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+
+full_name = last_name + ", " + first_name
+print("Formatted Name:", full_name)
+print("Length:", len(full_name))
