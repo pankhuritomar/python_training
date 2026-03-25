0dd81c4a1a4bdbb215f9f53960f4f0c883804bd2
Pankhuri Tomar <pankhuritomar711@gmail.com>
2026-03-25 12:23:00 +0530

    Update List_manipulation.py

--- part2/List_manipulation.py
+++ part2/List_manipulation.py
@@ -1,1 +1,23 @@
 
+
+to_do_list = []
+
+while True:
+    
+    cmd = input("What do you wanna do? : View / Add / Quit").lower()
+
+    if cmd=="add":
+        item = input("Sure! What do you wanna add?")
+        to_do_list.append(item)
+        print("Item added successfully!!")
+    elif cmd=="view":
+        if len(to_do_list)==0:
+            print("Sorry! Your to do list is empty..")
+        else:
+            for i in to_do_list:
+                print(i)
+    elif cmd=="quit":
+        print("Exiting..")
+        break
+    else :
+        print("Choose from the provided options only!")
